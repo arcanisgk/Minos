775f076d8d85a518e4d2345f6c349a849d49af75
Walter Nuñez <icarosnet@gmail.com>
2022-10-14 11:10:39 -0500

    last update Problem extension
    last update Problem extension

--- public/index.php
+++ public/index.php
@@ -1,21 +1,3 @@
 <?php
 
-
-//phpinfo();
-
-//echo 'Hello World your On GOOOO';
-
-
-try {
-    $db = new PDO(
-        'mysql:host=mysql:3306;dbname=minos',
-        'minos',
-        '1qazxsw22',
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        ]
-    );
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-
+echo 'Hello World';
